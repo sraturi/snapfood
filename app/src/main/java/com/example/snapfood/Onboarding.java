2dcdaa43860efe8587f043a4b881b5a1233cb3a7
Joshua Peng <46659122+JCODEPENG@users.noreply.github.com>
2020-02-08 16:01:23 -0800

    Replacing my old stuff

--- app/src/main/java/com/example/snapfood/Onboarding.java
+++ app/src/main/java/com/example/snapfood/Onboarding.java
@@ -3,12 +3,17 @@ package com.example.snapfood;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class Onboarding extends AppCompatActivity {
+    private ImageButton StartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding);
+
+        StartButton
+
     }
 }
